b8fb143ae4bf69f74f42b1e86e70a2636902e087
工作 <ghxuan@outlook.com>
2019-05-27 15:39:59 +0800

    模拟登陆

--- get_questions.py
+++ get_questions.py
@@ -1,5 +1,8 @@
 import json
 import requests
+from requests_toolbelt import MultipartEncoder
+
+from srv.settings import *
 
 
 class Leetcode:
@@ -10,70 +13,65 @@ class Leetcode:
         }
         self.leave = {1: '简单', 2: '中等', 3: '困难'}
         self.cookies = self.get_cookies()
-        self.hea['x-csrftoken'] = self.cookies.get('csrftoken')
         self.login()
+        for k, v in self.get_chinese().items():
+            print(k, v)
         pass
 
+    # 获取初次登陆的cookie
     def get_cookies(self):
         res = requests.get('https://leetcode-cn.com/accounts/login/?next=%2Fproblemset%2Fall%2F',
                            headers=self.hea,
                            verify=False)
         return res.cookies
 
-    # 获取中文标题和cookies/csrftoken
-    def get_chinese_and_cookies(self):
-        self.hea['content-type'] = 'application/json'
-        response = {"operationName": "getQuestionTranslation",
-                    "variables": {},
-                    "query":
-                        "query getQuestionTranslation($lang: String) "
-                        "{\n  translations: allAppliedQuestionTranslations(lang: $lang) "
-                        "{\n    title\n    question "
-                        "{\n      questionId\n      __typename\n    }\n    __typename\n  }\n}\n"}
+    # 获取后台题号和中文标题
+    def get_chinese(self):
+        hea = {
+            'x-csrftoken': self.cookies.get('csrftoken'),
+            'content-type': 'application/json',
+        }
+        hea.update(self.hea)
+        response = {
+            "operationName": "getQuestionTranslation",
+            "variables": {},
+            "query":
+                "query getQuestionTranslation($lang: String) "
+                "{\n  translations: allAppliedQuestionTranslations(lang: $lang) "
+                "{\n    title\n    question "
+                "{\n      questionId\n      __typename\n    }\n    __typename\n  }\n}\n"}
         res = requests.post('https://leetcode-cn.com/graphql',
-                            headers=self.hea,
+                            headers=hea,
+                            cookies=self.cookies,
                             verify=False,
                             data=json.dumps(response))
+        # print(res.text)
         return dict((
             (int(question['question']['questionId']), question['title'])
-            for question in json.loads(res.text)['data']['translations'])), res.cookies
+            for question in json.loads(res.text)['data']['translations']))
 
     # 模拟登陆  更换cookies
     def login(self):
-        self.hea['content-type'] = 'multipart/form-data; boundary=-----WebKitFormBoundaryETGpmlA09oWsdlBR'
-        self.hea['referer'] = 'https://leetcode-cn.com/accounts/login/'
-        self.hea['x-requested-with'] = 'XMLHttpRequest'
-        response = {
-            "csrfmiddlewaretoken": self.hea['x-csrftoken'],
-            "login": "709808634@qq.com",
-            "password": "709808634",
+        response = MultipartEncoder({
+            "csrfmiddlewaretoken": self.cookies.get('csrftoken'),
+            "login": login,
+            "password": password,
             "next": "/problems",
+        })
+        hea = {
+            'x-csrftoken': self.cookies.get('csrftoken'),
+            'content-type': response.content_type,
+            'referer': 'https://leetcode-cn.com/accounts/login/',
+            'x-requested-with': 'XMLHttpRequest',
         }
-        response = f"""------WebKitFormBoundaryETGpmlA09oWsdlBR
-Content-Disposition: form-data; name="csrfmiddlewaretoken"
-
-{self.hea['x-csrftoken']}
-------WebKitFormBoundaryETGpmlA09oWsdlBR
-Content-Disposition: form-data; name="login"
-
-709808634@qq.com
-------WebKitFormBoundaryETGpmlA09oWsdlBR
-Content-Disposition: form-data; name="password"
-
-709808634
-------WebKitFormBoundaryETGpmlA09oWsdlBR
-Content-Disposition: form-data; name="next"
-
-/problems
-------WebKitFormBoundaryETGpmlA09oWsdlBR--
-"""
-        print(response)
+        hea.update(self.hea)
         res = requests.post('https://leetcode-cn.com/accounts/login/',
-                            headers=self.hea,
+                            headers=hea,
                             cookies=self.cookies,
                             verify=False,
                             data=response)
-        print(res.text)
+        self.cookies = res.cookies
+        # print(res.text)
         pass
 
 
